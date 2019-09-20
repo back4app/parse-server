82d13416a75c91b7eab7fc9f44078e76338adde1
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2019-09-20 14:25:30 -0700

    Add default Relay schema

--- src/GraphQL/loaders/defaultRelaySchema.js
+++ src/GraphQL/loaders/defaultRelaySchema.js
@@ -1,3 +1,45 @@
-const load = () => {};
+import { nodeDefinitions, fromGlobalId } from 'graphql-relay';
+import getFieldNames from 'graphql-list-fields';
+import * as objectsQueries from './objectsQueries';
+import { extractKeysAndInclude } from './parseClassTypes';
+
+const load = parseGraphQLSchema => {
+  const { nodeInterface, nodeField } = nodeDefinitions(
+    async (globalId, context, queryInfo) => {
+      try {
+        const { type, id } = fromGlobalId(globalId);
+        const { config, auth, info } = context;
+        const selectedFields = getFieldNames(queryInfo);
+
+        const { keys, include } = extractKeysAndInclude(selectedFields);
+
+        return {
+          className: type,
+          ...(await objectsQueries.getObject(
+            type,
+            id,
+            keys,
+            include,
+            undefined,
+            undefined,
+            config,
+            auth,
+            info
+          )),
+        };
+      } catch (e) {
+        parseGraphQLSchema.handleError(e);
+      }
+    },
+    obj => {
+      return parseGraphQLSchema.parseClassTypes[obj.className]
+        .classGraphQLOutputType;
+    }
+  );
+
+  parseGraphQLSchema.relayNodeInterface = nodeInterface;
+  parseGraphQLSchema.addGraphQLType(nodeInterface, true);
+  parseGraphQLSchema.addGraphQLQuery('node', nodeField, true);
+};
 
 export { load };
